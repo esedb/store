7c8d8c32e8b6e4eb271006bd6aad8680635de7d1
unknown <ese.okoro@ebanqo.com>
2020-12-17 11:51:14 +0100

    updates

--- common/redis_conn.go
+++ common/redis_conn.go
@@ -4,10 +4,12 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
-func RedisObj () {
+func RedisObj() *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
+
+	return rdb
 }
